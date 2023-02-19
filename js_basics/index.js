4fb020b85d094c7df3167cef4e1c34b8602d86e4
Joy879 <joywanjiru879@gmail.com>
2023-02-19 21:00:39 +0300

    "Add files"

--- js_basics/index.js
+++ js_basics/index.js
@@ -3,10 +3,10 @@ function myFunction(p1, p2) {
     return p1 * p2 }
 console.log(myFunction(10, 20))
 //create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence. Use these variables to form a sentence and log the sentence in the console.
-const firstName = "Joy"
-const lastName = "Wanjiru"
-const StuternTrack = "Backend"
-const skills = ["HTML", "CSS", "JavaScript", "Python"]
-const favouriteColor = "black"
-const yearofIndependence = 1963
+const firstName = "Joy";
+const lastName = "Wanjiru";
+const StuternTrack = "Backend";
+const skills = ["HTML", "CSS", "JavaScript", "Python"];
+const favouriteColor = "black";
+const yearofIndependence = 1963;
 console.log(`My name is  ${firstName} ${lastName}. I am currently specializing in ${StuternTrack}. I am proficient in ${skills}. I love ${favouriteColor}. Our country gained independence in ${yearofIndependence}` )
